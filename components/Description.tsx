3134e70f4e871092e36d11adda631b872adddee1
Sam Samskies <samsamskies@gmail.com>
2023-03-19 12:27:28 -0500

    publish description

---
+++ components/Description.tsx
@@ -0,0 +1,16 @@
+import { type ReactNode } from "react";
+
+export const Description = ({ children }: { children: ReactNode }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#FFFFFF",
+        borderRadius: "8px",
+        boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
+        padding: "24px",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
---
+++ pages/api/users/[userId]/noost.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { createDescriptionNoost, publishNoost } from "@/utils";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { userId, content } = req.body;
+
+  switch (req.method) {
+    case "POST":
+      try {
+        const event = createDescriptionNoost({
+          userPubkey: userId as string,
+          content: content as string,
+          frenstrPubkey: process.env.FRENSTR_NOSTR_PUBLIC_KEY as string,
+          frenstrPrivkey: process.env.FRENSTR_NOSTR_PRIVATE_KEY as string,
+        });
+        const success = await publishNoost(event);
+
+        if (success) {
+          res.status(201).end("ok");
+        } else {
+          res.status(500).end("Something went wrong :(");
+        }
+      } catch (error) {
+        res.status(500).end("Something went wrong :(");
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
