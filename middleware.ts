bbed1cc320ab474ef96f79941fedc2fbf1449390
Sam Samskies <samsamskies@gmail.com>
2023-03-23 11:04:53 -0600

    add endpoint protection

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,44 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getIronSession } from "iron-session/edge";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(req: NextRequest) {
-  const isHostWhitelisted = () => {
-    for (const host of ["frenstr.com", "www.frenstr.com", "localhost:3000"]) {
-      if (
-        req.headers.get("host") === host ||
-        req.headers.get("host")?.endsWith("-samsamskies.vercel.app")
-      ) {
-        return true;
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const sessionPassword = process.env.SESSION_PASSWORD;
+
+  if (!sessionPassword) {
+    throw new Error("SESSION_PASSWORD is not defined.");
+  }
+
+  const session = await getIronSession(req, res, {
+    cookieName: "frenstr",
+    password: sessionPassword,
+  });
+
+  // @ts-ignore
+  const isAuthorized = () => {
+    let isTryingToAccessProtectedEndpoint = false;
+
+    for (const s in ["description", "noost"]) {
+      if (req.nextUrl.pathname.includes(s)) {
+        isTryingToAccessProtectedEndpoint = true;
+        break;
       }
     }
 
-    return false;
+    // @ts-ignore
+    return isTryingToAccessProtectedEndpoint && session.authorized === true;
   };
 
-  // TODO: implement a better way to lock endpoints down
-  if (!isHostWhitelisted()) {
+  if (!isAuthorized()) {
     return new NextResponse("Forbidden", { status: 403 });
   }
 
-  return NextResponse.next();
+  // @ts-ignore
+  session.authorized = true;
+  await session.save();
+
+  return res;
 }
 
 export const config = {
